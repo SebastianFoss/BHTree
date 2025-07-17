71acbbff4abebe5058120d2d924e563497fcdf0d
sfoss <sfoss@seattleu.edu>
2025-07-17 16:30:58 -0700

    Node -- friend class nodepool, getters, bools, updateMassAndCenterOfMass(particle)

--- Node.h
+++ Node.h
@@ -18,65 +18,109 @@ class Node {
 
 private:
     Box box;
-
     double totalMass;
     Vec centerOfMass;
+    Particle* particle; // points to a single particle if it is a leaf
+    std::array<Node*, 8> children; // Nodepool-managed children pointers
+
+    void updateMassAndCenterOfMass(const Particle* p) {
+        if (totalMass == 0.0) {
+            centerOfMass = p->getPos();
+            totalMass = p->getMass();
+        }
+        else {
+            centerOfMass = (centerOfMass * totalMass) + (p->getPos() * p->getMass()) / (totalMass + p->getMass());
+        }
+    }
 
 
-
-    // if one or no nodes
-    Particle* particle;
-
-    // used to keep track of initial particles for construction
-    std::vector<Particle*> particles_in_node_for_build;
-    // holds 0/1 particles for leafs, more for internals.
-    // checked by checker method
-
-    bool isLeaf;
-
-public:
-
+    // called only by NodePool
     // initializes Node with designated bounds as Box struct
-    Node(Box box)
+    Node(const Box& box)
         // initialze every member to zero -- all are contingent on adding particles
         : box(box),
-        isLeaf(true), // a new node starts as a potential leaf, and only becomes internal when subdivision is necessary
-        totalMass(0) {
+        totalMass(0), // a new node starts as a potential leaf, and only becomes internal when subdivision is necessary
+        particle(nullptr) {
         for (Node*& child: children) {
             child = nullptr;
         }
         // particles_in_node_for_build is null by default.
     };
 
+    friend class NodePool; // allows NodePool to do so
+
+public:
+
+    // getters
+    bool isLeaf() const {
+        return particle != nullptr;
+    }
+    bool isEmpty() const {
+        if (particle != nullptr) {
+            return false;
+        }
+        for (const Node* child: children) {
+            if (child != nullptr) {
+                return false;
+            }
+        }
+        return true;
+    }
+    const Box& getBox() {
+        return box;
+    }
+    const double getTotalMass() const {
+        return totalMass;
+    }
+    const Vec& getCenterOfMass() const {
+        return centerOfMass;
+    }
+    Node* getChild(int index) const {
+        return children[index];
+    }
+    const Particle* getParticle() const {
+        return particle;
+    }
+
     // for when a Node is reused from the pool
     // resets leaf status, clears particles, sets m to 0, deallocates arr
     // clears any state from its previous use in the tree.
-    void reset() {
-
-        // sets vector values to zero.
-        centerOfMass.reset();
-
-        // reset members
+    void reset(const Box& box) {
+        this->box = box;
         totalMass = 0;
-        isLeaf = true;
-
-        // CRUCIALLY: reset particles in node for build to zero. This ensures the particles are not re-initialized.
-        particles_in_node_for_build.clear();
-
-        // set all children node ptrs to nullptr
+        particle = nullptr;
+        centerOfMass = Vec();
         for (Node*& child: children) {
             child = nullptr;
         }
     };
 
 
-    // add a particle to the Node
-    // called in recursive function?
-    // move semantics?
-    // pre: assumes particle has a valid state
-    void addParticle(Particle* particle) {
-        // simply push it to the array
-        particles_in_node_for_build.push_back(particle);
+    void addParticle(Particle* newParticle, NodePool& pool) {
+        // first, update mass and center of mass
+        updateMassAndCenterOfMass(newParticle);
+
+        if (isLeaf()) {
+            if (particle == nullptr) {
+                // the node is currently empty so it becomes leaf
+                // now holds this particle as primary
+                particle = newParticle;
+            }
+            else {
+                // the node contains a particle
+                // move existing particle to appropriate child node
+
+                Particle* oldParticle = particle;
+                particle = nullptr;
+                // this particle is no longer a leaf
+
+                std::array<Box, 8> childBoxes = box.subdivide();
+
+
+
+            }
+        }
+
     };
 
     // the recursive function
